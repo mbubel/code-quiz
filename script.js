36c651879c9aff3232852efed9668e0fbf88fec5
Michael <michaelbubel.1989@gmail.com>
2020-10-23 13:46:43 -0700

    Added all five questions and working click events

---
+++ script.js
@@ -0,0 +1,125 @@
+
+// Question 1 click events
+$(document).ready(function () {
+  $("#Answer1").on("click", function () {
+    // Correct response when clicked
+    var correct = "Correct!";
+    $("#QuestionOneAnswer").text(correct);
+  });
+
+  $("#Answer2").on("click", function () {
+    // Incorrect response when clicked
+    var incorrect = "Incorrect!";
+    $("#QuestionOneAnswer").text(incorrect);
+  });
+  $("#Answer3").on("click", function () {
+    // Incorrect response when clicked
+    var incorrect = "Incorrect!";
+    $("#QuestionOneAnswer").text(incorrect);
+  });
+  $("#Answer4").on("click", function () {
+    // Incorrect response when clicked
+    var incorrect = "Incorrect!";
+    $("#QuestionOneAnswer").text(incorrect);
+  });
+});
+
+//Question 2 click events
+$(document).ready(function () {
+  $("#Answer5").on("click", function () {
+    // Correct response when clicked
+    var correct = "Correct!";
+    $("#QuestionTwoAnswer").text(correct);
+  });
+
+  $("#Answer6").on("click", function () {
+    // Incorrect response when clicked
+    var incorrect = "Incorrect!";
+    $("#QuestionTwoAnswer").text(incorrect);
+  });
+  $("#Answer7").on("click", function () {
+    // Incorrect response when clicked
+    var incorrect = "Incorrect!";
+    $("#QuestionTwoAnswer").text(incorrect);
+  });
+  $("#Answer8").on("click", function () {
+    // Incorrect response when clicked
+    var incorrect = "Incorrect!";
+    $("#QuestionTwoAnswer").text(incorrect);
+  });
+});
+
+//Question 3 click events
+$(document).ready(function () {
+  $("#Answer9").on("click", function () {
+    // Correct response when clicked
+    var correct = "Correct!";
+    $("#QuestionThreeAnswer").text(correct);
+  });
+
+  $("#Answer10").on("click", function () {
+    // Incorrect response when clicked
+    var incorrect = "Incorrect!";
+    $("#QuestionThreeAnswer").text(incorrect);
+  });
+  $("#Answer11").on("click", function () {
+    // Incorrect response when clicked
+    var incorrect = "Incorrect!";
+    $("#QuestionThreeAnswer").text(incorrect);
+  });
+  $("#Answer12").on("click", function () {
+    // Incorrect response when clicked
+    var incorrect = "Incorrect!";
+    $("#QuestionThreeAnswer").text(incorrect);
+  });
+});
+
+//Question 4 click events
+$(document).ready(function () {
+  $("#Answer13").on("click", function () {
+    // Correct response when clicked
+    var correct = "Correct!";
+    $("#QuestionFourAnswer").text(correct);
+  });
+
+  $("#Answer14").on("click", function () {
+    // Incorrect response when clicked
+    var incorrect = "Incorrect!";
+    $("#QuestionFourAnswer").text(incorrect);
+  });
+  $("#Answer15").on("click", function () {
+    // Incorrect response when clicked
+    var incorrect = "Incorrect!";
+    $("#QuestionFourAnswer").text(incorrect);
+  });
+  $("#Answer16").on("click", function () {
+    // Incorrect response when clicked
+    var incorrect = "Incorrect!";
+    $("#QuestionFourAnswer").text(incorrect);
+  });
+});
+
+//Question 5 click events
+$(document).ready(function () {
+  $("#Answer17").on("click", function () {
+    // Correct response when clicked
+    var correct = "Correct!";
+    $("#QuestionFiveAnswer").text(correct);
+  });
+
+  $("#Answer18").on("click", function () {
+    // Incorrect response when clicked
+    var incorrect = "Incorrect!";
+    $("#QuestionFiveAnswer").text(incorrect);
+  });
+  $("#Answer19").on("click", function () {
+    // Incorrect response when clicked
+    var incorrect = "Incorrect!";
+    $("#QuestionFiveAnswer").text(incorrect);
+  });
+  $("#Answer20").on("click", function () {
+    // Incorrect response when clicked
+    var incorrect = "Incorrect!";
+    $("#QuestionFiveAnswer").text(incorrect);
+  });
+});
